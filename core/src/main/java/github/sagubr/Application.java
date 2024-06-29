a3b95dfb925028bcc6b59d523517b67a08890c34
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-06-29 09:58:39 -0300

    first commit

---
+++ core/src/main/java/github/sagubr/Application.java
@@ -0,0 +1,10 @@
+package github.sagubr;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
