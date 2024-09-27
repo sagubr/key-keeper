ec7a27ee6840bf34ffe6a838e7069df0d90e4aed
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-09-26 21:30:48 -0300

    fix: Correção configuração swagger

--- src/main/java/github/sagubr/Application.java
+++ src/main/java/github/sagubr/Application.java
@@ -2,14 +2,31 @@ package github.sagubr;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.*;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
                 title = "key-keeper",
                 version = "0.0"
-        )
+        ),
+        servers = @Server(
+                url = "http://localhost:8080/"
+        ),
+        security = @SecurityRequirement(name = "Authorization")
 )
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "Authorization",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT"
+        )
+})
 public class Application {
 
     public static void main(String[] args) {
